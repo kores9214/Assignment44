ff2c41f46ac96358ed901f09fc9c977d220621a1
shivkore <kores9214@gmail.com>
2018-12-22 00:39:54 +0530

    Assignment44 added

---
+++ src/app/first-comp/first-comp.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-comp',
+  templateUrl: './first-comp.component.html',
+  styleUrls: ['./first-comp.component.css']
+})
+export class FirstCompComponent  {
+}
